ea805421407fdad5a275a31d5d8e7b0be9631c1f
Ludy Amano <ludy.amano@mirago.com.br>
2021-06-18 14:57:38 -0300

    rotas dos produtos

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const vm = new Vue({
         produtos: [],
         produto: false,
         carrinho: [],
-        mensagemAlerta:"Item adicionado",
+        mensagemAlerta: "Item adicionado",
         alertaAtivo: false
 
     },
@@ -66,30 +66,43 @@ const vm = new Vue({
             this.carrinho.splice(index, 1)
         },
 
-        checarLocalStorage(){
+        checarLocalStorage() {
             if (window.localStorage.carrinho) {
                 this.carrinho = JSON.parse(window.localStorage.carrinho)
                 // volta ao que era antes de ser string
             }
         },
 
-        alerta(mensagem){
+        alerta(mensagem) {
             this.mensagemAlerta = mensagem;
             this.alertaAtivo = true;
 
-            setTimeout(() => { this.alertaAtivo = false}, 1500)
-        }
+            setTimeout(() => { this.alertaAtivo = false }, 1500)
+        },
+
+        router(){
+            const hash = document.location.hash;
+            if(hash) 
+            this.fetchProduto(hash.replace("#",""))
+        },
     },
 
-    watch:{
-        carrinho(){
-            window.localStorage.carrinho =  JSON.stringify(this.carrinho)   
+    watch: {
+        produto() {
+            document.title = this.produto.nome || "Techno";
+            const hash = this.produto.id || "Techno"
+            history.pushState(null, null, `#${hash}`)
+        },
+
+        carrinho() {
+            window.localStorage.carrinho = JSON.stringify(this.carrinho)
             // transforma e string
         }
     },
 
     created() {
-        this.fetchProdutos()
-        this.checarLocalStorage()
+        this.fetchProdutos();
+        this.router();
+        this.checarLocalStorage();
     }
 })
